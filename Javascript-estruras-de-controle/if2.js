baad61d302b03869d343baa401b415b3fc41a057
Matheus Santos Guedes <68879085+matheussantosguedes00@users.noreply.github.com>
2022-02-11 17:47:40 -0300

    up

---
+++ Javascript-estruras-de-controle/if2.js
@@ -0,0 +1,16 @@
+function teste (num){
+    if(num >7)
+    console.log(num)
+    console.log('Final')
+}
+teste(6)
+teste(8)
+
+function teste2(num){
+    if(num>7);{ //cuidado com ';', não usar com as estruturas de controle
+        
+    console.log(num)
+    }
+}
+teste2(6)
+teste2(8)
