7a8ec3fd829d7df44ca2088b870be20fbd2233e1
Matheus Santos Guedes <68879085+matheussantosguedes00@users.noreply.github.com>
2022-04-08 14:53:24 -0300

    up

---
+++ html/Execicios/formulario/server.js
@@ -0,0 +1,19 @@
+const express=require('express')
+const app =express()
+const bodyParse=require('body-parser')
+
+app.use(bodyParse.urlencoded({extended:true}))
+
+app.post('/usuarios',(req,resp)=>{
+    console.log(req.body)
+    
+    resp.send('<h1>Parabéns. Usuário Incluido</h1>')
+})
+app.post('/usuarios/:id',(req,resp)=>{
+    console.log(req.params.id)
+    console.log(req.body)
+    resp.send('<h1>Parabéns. Usuário Alterado ! </h1>')
+})
+app.listen(3003)
+
+
